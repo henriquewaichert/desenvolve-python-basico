8b4b50dbfa425d1ffc1dfa26e57b7bb42190b3ec
Henrique Waichert Rodrigues <153193699+henriquewaichert@users.noreply.github.com>
2024-04-18 15:26:48 -0300

    Add files via upload

---
+++ modulo2/Tipo de dado/aula2_questao2.py
@@ -0,0 +1,15 @@
+# 2) Transforme o seguinte algoritmo em código. Lembre-se das funções print e type que já conhecemos de forma geral.
+
+# Armazene o texto "o resultado é"  em uma variável, o valor 10 em outra variável, e o valor 3.5 numa terceira variável.
+# Some os valores da segunda e terceira variável e armazene em outra variável.
+# Imprima todas as variáveis na ordem de criação e imprima também seus tipos.
+
+resultado = "O resultado é"
+n1 = 10
+n2 = 3.5
+soma = (n1 + n2)
+
+print("Variável:", resultado, ", e o seu tipo: ", type(resultado))
+print("Variável:", n1, ", e o seu tipo: ", type(n1))
+print("Variável:", n2, ", e o seu tipo: ", type(n2))
+print("Variável:", soma, ", e o seu tipo: ", type(soma))
