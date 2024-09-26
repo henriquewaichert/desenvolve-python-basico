9627cbd2015a7f095340fc90cda1a7e657468e77
Henrique Waichert Rodrigues <153193699+henriquewaichert@users.noreply.github.com>
2024-09-26 18:14:06 -0300

    Add files via upload

---
+++ modulo2/aula2_questao1.py
@@ -0,0 +1,15 @@
+# 1) Transforme o seguinte algoritmo em código. Lembre-se das funções print e type que já conhecemos de forma geral.
+
+# Armazene o valor 5 em uma variável e o valor 2 em outra variável
+variavel1 = 5
+variavel2 = 2
+
+# Imprima o tipo de dados dessas duas variáveis
+print("O tipo de dados da variavel1:", type(variavel1))
+print("O tipo de dados da variavel2:", type(variavel2))
+
+# Divida a primeira variável (com o valor 5) pela segunda variável (com o valor 2) e armazene o resultado em uma terceira variável
+resultado_divisao = variavel1 / variavel2
+
+# Imprima a terceira variável e também o seu tipo
+print("O resultado da divisão das duas variaveis é:",resultado_divisao, "e o seu tipo de dado é:", type(resultado_divisao))
---
+++ modulo2/aula2_questao2.py
@@ -0,0 +1,15 @@
+# 2) Transforme o seguinte algoritmo em código. Lembre-se das funções print e type que já conhecemos de forma geral.
+    
+# Armazene o texto "o resultado é"  em uma variável, o valor 10 em outra variável, e o valor 3.5 numa terceira variável.
+texto = "O resultado é"
+valor1 = 10
+valor2 = 3.5
+
+# Some os valores da segunda e terceira variável e armazene em outra variável.
+soma_valores = valor1 + valor2
+
+# Imprima todas as variáveis na ordem de criação e imprima também seus tipos.
+print("Primeira variável: ", texto , " e seu tipo:", type(texto))
+print("Segunda variável: ", valor1, " e seu tipo:", type(valor1))
+print("Terceira variável: ", valor2, " e seu tipo:", type(valor2))
+print("Quarta variável: ", soma_valores, " e seu tipo:", type(soma_valores))
